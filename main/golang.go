8b2496b6180c127596fb312f2623db57143f3952
jek_a709 <evgenijblagun13@gmail.com>
2024-08-24 23:53:55 +0300

    upd

--- main/golang.go
+++ main/golang.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"net/http"
 	"text/template"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 func index(w http.ResponseWriter, r *http.Request) {
@@ -16,9 +19,47 @@ func index(w http.ResponseWriter, r *http.Request) {
 	t.ExecuteTemplate(w, "index", nil)
 }
 
+func register(w http.ResponseWriter, r *http.Request) {
+	t, err := template.ParseFiles("templates/register.html", "templates/header.html", "templates/footer.html")
+
+	if err != nil {
+		fmt.Fprintf(w, err.Error())
+	}
+
+	t.ExecuteTemplate(w, "register", nil)
+}
+
+func reg_user(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+
+	username := r.FormValue("username")
+	password := r.FormValue("password")
+
+	// username = "test"
+
+	fmt.Println(username + password)
+
+	db, err := sql.Open("mysql", "root:root@tcp(127.0.0.1:8889)/golang")
+	if err != nil {
+		panic(err)
+	}
+
+	defer db.Close()
+
+	insert, err := db.Query(fmt.Sprintf("INSERT INTO `all_users` (`name`, `password`) VALUES ('%s', '%s')", username, password))
+	if err != nil {
+		panic(err)
+	}
+	defer insert.Close()
+
+	http.Redirect(w, r, "/", http.StatusSeeOther)
+}
+
 func handleFunc() {
-	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./static"))))
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./static/"))))
 	http.HandleFunc("/", index)
+	http.HandleFunc("/register/", register)
+	http.HandleFunc("/reg_user/", reg_user)
 	http.ListenAndServe(":8080", nil)
 }
 
